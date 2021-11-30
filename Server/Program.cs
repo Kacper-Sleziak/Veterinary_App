0501df8eb32c95464ae87ba691883abf5b69612a
Michał <mdembinski2000@gmail.com>
2021-11-30 17:36:55 +0100

    Renamed Main class

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Server.Database;
 
 namespace Server
 {
-    class Main
+    class Program
     {
+        static void Main()
+        {
+
+        }
     }
 }
