fe95777bd97f9e0013b01408880b3f3093da7880
Michał <mdembinski2000@gmail.com>
2021-11-30 17:59:57 +0100

    Created test for registration

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,11 @@ namespace Server
 {
     class Program
     {
+        public static Repository _repository = new Repository();
         static void Main()
         {
-
+            Console.Write(_repository.RegisterUser("Adminator", "Adminarski", "Adam", "Adaasdmski", new DateTime(2000, 11, 15), "aaa@o2.pl", 123456789));
+            Console.Read();
         }
     }
 }
