3d298f0b8ea24873248a36ab1d53e24ac3db8373
Kacper Śleziak <kacper-sleziak@wp.pl>
2021-12-02 23:40:03 +0100

    AnimalCrud Update

--- Server/Database/AnimalsCrud.cs
+++ Server/Database/AnimalsCrud.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Server.Database
 {
     class AnimalsCrud
-    {
+    {    // A variable that allows you to connect to database
+        private SqlConnection _connection = new SqlConnection(Properties.Resources.ConnectionString);
+        
+        //Get animals by id of owner's personal data
+        public string GetAnimalsOfOwner(string ownerID)
+        {
+            using (_connection = new SqlConnection(Properties.Resources.ConnectionString))
+            {
+                string getAnimalsQuery = "SELECT * " +
+                                          "FROM Animals " +
+                                          $"Where OwnerID = '{ownerID}';";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(getAnimalsQuery, _connection);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                string temp = JsonConvert.SerializeObject(table);
+
+                return temp;
+            }
+        }
+
+        //Change data animal data by animal's id
+        public void ChangeAnimalData(string newName, string newWeight, string animalId)
+        {
+            using (_connection = new SqlConnection(Properties.Resources.ConnectionString))
+            {
+                _connection.Open();
+                string changeAnimalNameQuery = "UPDATE Animals " +
+                                               $"SET Name =   '{newName}' "   +
+                                               $",Weight = '{newWeight}' " +
+                                               $"Where Id =   '{animalId}';";
+
+                SqlCommand commandChangeAnimalName = new SqlCommand(changeAnimalNameQuery, _connection);
+                commandChangeAnimalName.ExecuteNonQuery();
+
+            }
+        }
     }
 }
