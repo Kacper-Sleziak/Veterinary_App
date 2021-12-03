0bb358f302c8cf704e509fee4a79ec3c8b958627
Kacper Śleziak <kacper-sleziak@wp.pl>
2021-12-03 11:29:13 +0100

    AnimalCrud almost done

--- Server/Database/AnimalsCrud.cs
+++ Server/Database/AnimalsCrud.cs
@@ -1,11 +1,14 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using System.ComponentModel;
+using Newtonsoft.Json;
 using System.Data;
 using System.Data.SqlClient;
 
 namespace Server.Database
 {
     class AnimalsCrud
-    {    // A variable that allows you to connect to database
+    {    
+        // A variable that allows you to connect to database
         private SqlConnection _connection = new SqlConnection(Properties.Resources.ConnectionString);
         
         //Get animals by id of owner's personal data
@@ -28,21 +31,52 @@ namespace Server.Database
         }
 
         //Change data animal data by animal's id
-        public void ChangeAnimalData(string newName, float newWeight, string newSpieces, int newOwnerId , string animalId)
+        public void ChangeAnimalData(string newName, float newWeight, string newSpecies, int newOwnerId , int animalId)
         {
             using (_connection = new SqlConnection(Properties.Resources.ConnectionString))
             {
-                _connection.Open();
+                
                 string changeAnimalNameQuery = "UPDATE Animals " +
-                                               $"SET Name =   '{newName}' "   +
-                                               $",Weight = '{newWeight}' " +
-                                               $",Spiecies = '{newSpieces}" +
-                                               $",OwnerId = '{newOwnerId}" +
-                                               $"Where Id =   '{animalId}';";
-
+                                               $"SET Name =  '{newName}', "   +
+                                               $"Weight = {newWeight}, " +
+                                               $"Species = '{newSpecies}', " +
+                                               $"OwnerId = {newOwnerId} " +
+                                               $"Where Id =   {animalId};";
+                _connection.Open();
                 SqlCommand commandChangeAnimalName = new SqlCommand(changeAnimalNameQuery, _connection);
                 commandChangeAnimalName.ExecuteNonQuery();
 
+            }
+        }
+
+        //Delete animal from data base by animal's id
+        public void DeleteAnimal(int animalId)
+        {
+            using (_connection = new SqlConnection(Properties.Resources.ConnectionString))
+            {
+                string deleteAnimalQuery = "DELETE FROM Animals " +
+                                           $"WHERE Id = {animalId};";
+
+                _connection.Open();
+                SqlCommand deleteAnimalCommand = new SqlCommand(deleteAnimalQuery, _connection);
+                deleteAnimalCommand.ExecuteNonQuery();
+            }
+        }
+
+        //Add new animals to data base
+        
+        //Problem with using float typical float number as a parameter of weight for example 20.1,
+        //Normal int passed as a weight is working correctly 
+        public void AddNewAnimal(string name, string species, float weight, int ownerId)
+        {
+            using (_connection = new SqlConnection(Properties.Resources.ConnectionString))
+            {
+                string addAnimalQuery = "INSERT INTO Animals " +
+                                        $"VALUES ('{name}', '{species}', {weight}, {ownerId});";
+
+                _connection.Open();
+                SqlCommand addAnimalCommand = new SqlCommand(addAnimalQuery, _connection);
+                addAnimalCommand.ExecuteNonQuery();
             }
         }
     }
