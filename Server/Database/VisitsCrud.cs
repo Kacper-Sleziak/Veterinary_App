007d068d69ec762b5d687e438f57e83db487fc9f
Kacper <kacper-sleziak@wp.pl>
2021-12-13 23:34:03 +0100

    Update VisitsCrud

--- Server/Database/VisitsCrud.cs
+++ Server/Database/VisitsCrud.cs
@@ -2,5 +2,80 @@
 {
     class VisitsCrud
     {
+        // A variable that allows you to connect to database
+        private SqlConnection _connection = new SqlConnection(Properties.Resources.ConnectionString);
+
+        //Function add new visit to database
+        public bool AddVisit(DateTime date, int visitTypeId, int animalId, int vetId)
+        {
+            using (_connection = new SqlConnection(Properties.Resources.ConnectionString))
+            {
+                _connection.Open();
+                string addVisitQuery = "INSERT INTO Visits " +
+                                          $"VALUES ({date}, {visitTypeId}, {animalId}, {vetId});";
+                SqlCommand command = new SqlCommand(addVisitQuery, _connection);
+                try
+                {
+                    command.ExecuteNonQuery();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
+                    Console.WriteLine("  Message: {0}", ex.Message);
+                    return false;
+                }
+            }
+        }
+
+        //Function update visit
+        public bool UpdateVisit(int id, DateTime date, int visitTypeId, int animalId, int vetId)
+        {
+            using (_connection = new SqlConnection(Properties.Resources.ConnectionString))
+            {
+                _connection.Open();
+                string updateVisitQuery = "UPDATE Visits " +
+                                             $"SET Date =  {date}, " +
+                                             $"VisitTypeId = {visitTypeId} " +
+                                             $"AnimalId = {animalId}" +
+                                             $"VetId = {vetId}" +
+                                             $"WHERE Id = {id};";
+                SqlCommand command = new SqlCommand(updateVisitQuery, _connection);
+                try
+                {
+                    command.ExecuteNonQuery();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
+                    Console.WriteLine("  Message: {0}", ex.Message);
+                    return false;
+                }
+            }
+        }
+
+        //Function delete visit from database
+        public bool DeleteEmployee(int id)
+        {
+            using (_connection = new SqlConnection(Properties.Resources.ConnectionString))
+            {
+                _connection.Open();
+                string deleteVisitQuery = "DELETE Visits " +
+                                          $"Where Id = {id};";
+                SqlCommand command = new SqlCommand(deleteVisitQuery, _connection);
+                try
+                {
+                    command.ExecuteNonQuery();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
+                    Console.WriteLine("  Message: {0}", ex.Message);
+                    return false;
+                }
+            }
+        }
     }
 }
