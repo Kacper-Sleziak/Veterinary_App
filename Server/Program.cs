4146ffc8dd5af7bd5b8bd6f6f8a77b4ae124610e
Michał <mdembinski2000@gmail.com>
2021-12-16 23:14:04 +0100

    Small changes in code structure, solved list parsing problem

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -8,7 +9,7 @@ using Server.Database;
 
 namespace Server
 {
-    class Program
+    internal class Program
     {
         public static Transactions _repository = new Transactions();
         public static PersonalDataCrud _personalData = new PersonalDataCrud();
@@ -25,19 +26,19 @@ namespace Server
             public const int BufferSize = 1024;
 
             // Receive buffer.  
-            public byte[] buffer = new byte[BufferSize];
+            public byte[] Buffer = new byte[BufferSize];
 
             // Received data string.
-            public StringBuilder sb = new StringBuilder();
+            public StringBuilder Sb = new StringBuilder();
 
             // Client socket.
-            public Socket workSocket = null;
+            public Socket WorkSocket = null;
         }
 
         public class AsynchronousSocketListener
         {
             // Thread signal.  
-            public static ManualResetEvent allDone = new ManualResetEvent(false);
+            public static ManualResetEvent AllDone = new ManualResetEvent(false);
 
             public AsynchronousSocketListener()
             {
@@ -48,12 +49,12 @@ namespace Server
                 // Establish the local endpoint for the socket.  
                 // The DNS name of the computer  
                 // running the listener is "host.contoso.com".  
-                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-                IPAddress ipAddress = ipHostInfo.AddressList[0];
-                IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+                var ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                var ipAddress = ipHostInfo.AddressList[0];
+                var localEndPoint = new IPEndPoint(ipAddress, 11000);
 
                 // Create a TCP/IP socket.  
-                Socket listener = new Socket(ipAddress.AddressFamily,
+                var listener = new Socket(ipAddress.AddressFamily,
                     SocketType.Stream, ProtocolType.Tcp);
 
                 // Bind the socket to the local endpoint and listen for incoming connections.  
@@ -65,7 +66,7 @@ namespace Server
                     while (true)
                     {
                         // Set the event to nonsignaled state.  
-                        allDone.Reset();
+                        AllDone.Reset();
 
                         // Start an asynchronous socket to listen for connections.  
                         Console.WriteLine("Waiting for a connection...");
@@ -74,7 +75,7 @@ namespace Server
                             listener);
 
                         // Wait until a connection is made before continuing.  
-                        allDone.WaitOne();
+                        AllDone.WaitOne();
                     }
 
                 }
@@ -91,143 +92,125 @@ namespace Server
             public static void AcceptCallback(IAsyncResult ar)
             {
                 // Signal the main thread to continue.  
-                allDone.Set();
+                AllDone.Set();
 
                 // Get the socket that handles the client request.  
-                Socket listener = (Socket)ar.AsyncState;
-                Socket handler = listener.EndAccept(ar);
+                var listener = (Socket)ar.AsyncState;
+                var handler = listener.EndAccept(ar);
 
                 // Create the state object.  
-                StateObject state = new StateObject();
-                state.workSocket = handler;
-                handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                var state = new StateObject {WorkSocket = handler};
+                handler.BeginReceive(state.Buffer, 0, StateObject.BufferSize, 0,
                     new AsyncCallback(ReadCallback), state);
             }
 
             public static void ReadCallback(IAsyncResult ar)
             {
-                String content = String.Empty;
-
                 // Retrieve the state object and the handler socket  
                 // from the asynchronous state object.  
-                StateObject state = (StateObject)ar.AsyncState;
-                Socket handler = state.workSocket;
+                var state = (StateObject)ar.AsyncState;
+                var handler = state.WorkSocket;
 
                 // Read data from the client socket.
-                int bytesRead = handler.EndReceive(ar);
+                var bytesRead = handler.EndReceive(ar);
 
-                if (bytesRead > 0)
+                if (bytesRead <= 0) return;
+                // There  might be more data, so store the data received so far.  
+                state.Sb.Append(Encoding.ASCII.GetString(
+                    state.Buffer, 0, bytesRead));
+
+                // Check for end-of-file tag. If it is not there, read
+                // more data.  
+                var content = state.Sb.ToString();
+                if (content.IndexOf("<EOF>") > -1)
                 {
-                    // There  might be more data, so store the data received so far.  
-                    state.sb.Append(Encoding.ASCII.GetString(
-                        state.buffer, 0, bytesRead));
+                    var result = "<EOF>";
+                    // All the data has been read from the
+                    // client. Display it on the console.  
+                    Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
+                        content.Length, content);
 
-                    // Check for end-of-file tag. If it is not there, read
-                    // more data.  
-                    content = state.sb.ToString();
-                    if (content.IndexOf("<EOF>") > -1)
+                    ////////////////////////////////////////
+                    //tutaj switch , jaki content (dane od klienta) odpowiada jakiej funkcji i co wysłać
+                    char[] separator = { '(', ',', ')' };
+                    var function = content.Split(separator);
+
+                    switch (function[0])
                     {
-                        String result = "<EOF>";
-                        // All the data has been read from the
-                        // client. Display it on the console.  
-                        Console.WriteLine("Read {0} bytes from socket. \n Data : {1}",
-                            content.Length, content);
-
-                        ////////////////////////////////////////
-                        //tutaj ify , jaki content (dane od klienta) odpowiada jakiej funkcji i co wysłać
-                        char[] spearator = { '(', ',', ')' };
-                        var function = content.Split(spearator);
-
-                        if (function[0] == "Login")
-                        {
+                        case "Login":
                             result = _repository.Login(function[1], function[2]).ToString();
-                        }
-
-                        else if (function[0] == "Register")
+                            break;
+                        //Register for client
+                        case "Register" when result.Length == 9:
+                            result = _repository.Register(function[1], function[2], function[3], function[4],
+                                DateTime.Parse(function[5]), function[6], int.Parse(function[7])).ToString();
+                            break;
+                        case "Register":
                         {
-
-                            //Register for client
-                            if (result.Length == 9)
+                            if (result.Length == 10)
                             {
                                 result = _repository.Register(function[1], function[2], function[3], function[4],
-                                 DateTime.Parse(function[5]), function[6], Int32.Parse(function[7])).ToString();
+                                    DateTime.Parse(function[5]), function[6], int.Parse(function[7]), function[8]).ToString();
                             }
 
-                            //Register for employe
-                            else if (result.Length == 10)
-                            {
-                                result = _repository.Register(function[1], function[2], function[3], function[4],
-                                    DateTime.Parse(function[5]), function[6], Int32.Parse(function[7]), function[8]).ToString();
-                            }
-
-                            
+                            break;
                         }
-                        
-                        else if (function[0] == "GetPersonalData")
+                        case "GetPersonalData":
+                            result = _personalData.GetPersonalData(int.Parse(function[1]));
+                            break;
+                        //Klient przekazuje listę w postaci [el1;el2;el3;el4...]
+                        case "AddNewVisit":
+                            result = _repository.addNewVisit(int.Parse(function[1]), int.Parse(function[2]), int.Parse(function[3]), int.Parse(function[4])).ToString();
+                            break;
+                        case "Order":
                         {
-                            result = _personalData.GetPersonalData(Int32.Parse(function[1]));
+                            char[] listSeparator = { '[', ';', ']' };
+                            var ProductId =
+                                function[9].Split(listSeparator).ToList().ConvertAll(input => int.Parse(input));
+                            var ProductAmount =
+                                function[10].Split(listSeparator).ToList().ConvertAll(input => int.Parse(input));
+                            result = _repository.Order(int.Parse(function[1]), DateTime.Parse(function[2]), function[3], int.Parse(function[4]), function[5],
+                                function[6], function[7], function[8], ProductId, ProductAmount).ToString();
+                            break;
                         }
-                        
-                        else if (function[0] == "AddNewVisit")
-                        {
-                            result = _repository.addNewVisit(Int32.Parse(function[1]), Int32.Parse(function[2]), Int32.Parse(function[3]), Int32.Parse(function[4])).ToString();
-                        }
-
-
-                        //Klopty z konwertowaniem stringa do listy typu int
-                        else if (function[0] == "Order")
-                        {
-                            result = _repository.Order(Int32.Parse(function[1]), DateTime.Parse(function[2]), function[3], Int32.Parse(function[4]), function[5],
-                                function[6], function[7], function[8], function[9], function[10]).ToString();
-                        }
-
-                        else if (function[0] == "CancelVisit")
-                        {
-                            result = _repository.CancelVisit(Int32.Parse(function[1])).ToString();             
-                        }
-
-                        else if (function[0] == "ChangePassword")
-                        {
-                            result = _credentials.UpdateCredentials(Int32.Parse(function[1]), function[2], function[3],
-                                Int32.Parse(function[4])).ToString();
-                        }
-
-                        else if (function[0] == "GetAnimals")
-                        {
-                            result = _animals.GetAnimalsOfOwner(Int32.Parse(function[1]));
-                        }
-
-                        else if (function[0] == "GetVisitsOfAnimal")
-                        {
-                            result = _Visits.GetAnimalVisits(Int32.Parse(function[1])).ToString();
-                        }
-
-                        else if (function[0] == "GetVetVisits")
-                        {
-                            result = _Visits.GetVetVisits(Int32.Parse(function[1])).ToString();
-                        }
-
-
-                        result += "<EOF>";
-
-                        ////////////////////////////////////////
-
-                        // Echo the data back to the client.
-                        Send(handler, result);
+                        case "CancelVisit":
+                            result = _repository.CancelVisit(int.Parse(function[1])).ToString();
+                            break;
+                        case "ChangePassword":
+                            result = _credentials.UpdateCredentials(int.Parse(function[1]), function[2], function[3],
+                                int.Parse(function[4])).ToString();
+                            break;
+                        case "GetAnimals":
+                            result = _animals.GetAnimalsOfOwner(int.Parse(function[1]));
+                            break;
+                        case "GetVisitsOfAnimal":
+                            result = _Visits.GetAnimalVisits(int.Parse(function[1])).ToString();
+                            break;
+                        case "GetVetVisits":
+                            result = _Visits.GetVetVisits(int.Parse(function[1])).ToString();
+                            break;
                     }
-                    else
-                    {
-                        // Not all data received. Get more.  
-                        handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+
+
+                    result += "<EOF>";
+
+                    ////////////////////////////////////////
+
+                    // Echo the data back to the client.
+                    Send(handler, result);
+                }
+                else
+                {
+                    // Not all data received. Get more.  
+                    handler.BeginReceive(state.Buffer, 0, StateObject.BufferSize, 0,
                         new AsyncCallback(ReadCallback), state);
-                    }
                 }
             }
 
             private static void Send(Socket handler, String data)
             {
                 // Convert the string data to byte data using ASCII encoding.  
-                byte[] byteData = Encoding.ASCII.GetBytes(data);
+                var byteData = Encoding.ASCII.GetBytes(data);
 
                 // Begin sending the data to the remote device.  
                 handler.BeginSend(byteData, 0, byteData.Length, 0,
@@ -239,10 +222,10 @@ namespace Server
                 try
                 {
                     // Retrieve the socket from the state object.  
-                    Socket handler = (Socket)ar.AsyncState;
+                    var handler = (Socket)ar.AsyncState;
 
                     // Complete sending the data to the remote device.  
-                    int bytesSent = handler.EndSend(ar);
+                    var bytesSent = handler.EndSend(ar);
                     Console.WriteLine("Sent {0} bytes to client.", bytesSent);
 
                     handler.Shutdown(SocketShutdown.Both);
@@ -255,7 +238,7 @@ namespace Server
                 }
             }
 
-            public static int Main(String[] args)
+            public static int Main(string[] args)
             {
                 StartListening();
                 return 0;
