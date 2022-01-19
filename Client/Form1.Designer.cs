7ed2b945a4e434ef3fb526eec274ae9b6a0f29a9
Kacper <kacper-sleziak@wp.pl>
2022-01-19 21:50:09 +0100

    Push

--- Client/Form1.Designer.cs
+++ Client/Form1.Designer.cs
@@ -30,24 +30,59 @@ namespace Client
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(14, 16);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.Size = new System.Drawing.Size(114, 29);
             this.textBox1.TabIndex = 0;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(405, 205);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(124, 43);
+            this.buttonLogin.TabIndex = 1;
+            this.buttonLogin.Text = "Zaloguj";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(402, 105);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(127, 27);
+            this.textBoxLogin.TabIndex = 2;
+            this.textBoxLogin.TextChanged += new System.EventHandler(this.textBoxLogin_TextChanged);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(402, 154);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(127, 27);
+            this.textBoxPassword.TabIndex = 3;
+            this.textBoxPassword.TextChanged += new System.EventHandler(this.textBoxPassword_TextChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.buttonLogin);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +91,9 @@ namespace Client
         #endregion
 
         public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
     }
 }
 
--- Client/Form1.cs
+++ Client/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Net;
@@ -9,6 +10,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace Client
 {
@@ -17,10 +19,9 @@ namespace Client
         public Form1()
         {
             InitializeComponent();
-            StartClient();
         }
 
-        public void StartClient()
+        public string StartClient(string message)
         {
             // Data buffer for incoming data.  
             byte[] bytes = new byte[1024];
@@ -46,22 +47,23 @@ namespace Client
                     Console.WriteLine("Socket connected to {0}",
                         sender.RemoteEndPoint.ToString());
 
-                    // Encode the data string into a byte array.  
-                    byte[] msg = Encoding.ASCII.GetBytes("Login(Bartek12, lol123)<EOF>");
+                    // Encode the data string into a byte array. 
+                    byte[] msg = Encoding.ASCII.GetBytes(message);
 
                     // Send the data through the socket.  
                     int bytesSent = sender.Send(msg);
 
                     // Receive the response from the remote device.  
                     int bytesRec = sender.Receive(bytes);
-                    Console.WriteLine("Echoed test = {0}",
+                    Console.WriteLine("Echoed test = {0}", 
                         Encoding.ASCII.GetString(bytes, 0, bytesRec));
-                    textBox1.Text = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
                     // Release the socket.  
                     sender.Shutdown(SocketShutdown.Both);
                     sender.Close();
-
+                    return Encoding.ASCII.GetString(bytes, 0, bytesRec);
                 }
+
                 catch (ArgumentNullException ane)
                 {
                     Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
@@ -80,6 +82,42 @@ namespace Client
             {
                 Console.WriteLine(e.ToString());
             }
+
+            return null;
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string login = textBoxLogin.Text;
+            string password = textBoxPassword.Text;
+            string msg_string = "Login(";
+
+
+            //string "Login(login, password)<EOF>"
+            msg_string += login;
+            msg_string += ", ";
+            msg_string += password;
+            msg_string += ")<EOF>";
+
+            var returnedString = StartClient(msg_string);
+            textBox1.Text = returnedString;
+
+        }
+
+        private void textBoxPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxLogin_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
