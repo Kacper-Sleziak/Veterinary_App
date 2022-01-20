926b4c6760eeba29ece7b99fd51968d39f7d6fac
Kacper <kacper-sleziak@wp.pl>
2022-01-20 21:11:39 +0100

    Folders Update

---
+++ Client/UsersForms/FormShowVisits.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Client.UsersForms
+{
+    public partial class FormShowVisits : Form
+    {
+        public FormShowVisits()
+        {
+            InitializeComponent();
+        }
+
+        private void labelVisits_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBoxVisits_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
